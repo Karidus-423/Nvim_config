7076615a45f17cad0428d95052d929a5045b52d5
Karidus-423 <kapuerto23@gmail.com>
2024-01-20 12:52:31 -0500

    Lsp-zero
    For automatic download of LSP.

--- lua/karidus/plugins/noice.lua
+++ lua/karidus/plugins/noice.lua
@@ -1,5 +1,5 @@
+-- lazy.nvim
 return {
-	-- lazy.nvim
 	"folke/noice.nvim",
 	event = "VeryLazy",
 	opts = {
@@ -10,14 +10,20 @@ return {
 				["vim.lsp.util.stylize_markdown"] = true,
 				["cmp.entry.get_documentation"] = true,
 			},
+			hover = {
+				enabled = false,
+			},
+			signature = {
+				enabled = false,
+			},
 		},
 		-- you can enable a preset for easier configuration
 		presets = {
-			bottom_search = false, -- use a classic bottom cmdline for search
+			bottom_search = true, -- use a classic bottom cmdline for search
 			command_palette = true, -- position the cmdline and popupmenu together
 			long_message_to_split = false, -- long messages will be sent to a split
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = true, -- add a border to hover docs and signature help
+			lsp_doc_border = false, -- add a border to hover docs and signature help
 		},
 	},
 	dependencies = {
--- lua/karidus/plugins/rustacean.lua
+++
@@ -1,5 +0,0 @@
-return {
-	"mrcjkb/rustaceanvim",
-	version = "^3",
-	ft = { "rust" }, -- is enabled on rust files
-}
