707e9ea237d191caf05853e05f7faa6f038f039a
Karidus-423 <kapuerto23@gmail.com>
2024-02-22 20:12:17 -0500

    norg/wsl config

---
+++ lua/karidus/plugins/mini.lua
@@ -0,0 +1,6 @@
+return {
+    { 'echasnovski/mini.notify',      version = false, opts = {} },
+    { 'echasnovski/mini.map',         version = '*',   opts = {} },
+    { 'echasnovski/mini.indentscope', version = false, opts = {} },
+    { 'echasnovski/mini.map',         version = '*',   opts = {} },
+}
