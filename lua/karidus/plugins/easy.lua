d08bac37ba069b7f7444e15284a1205b817e3c72
Karidus-423 <kapuerto23@gmail.com>
2024-03-02 21:38:30 -0500

    removed statusline

--- lua/karidus/plugins/easy.lua
+++ lua/karidus/plugins/easy.lua
@@ -2,5 +2,16 @@ return {
     { "windwp/nvim-autopairs",     event = "InsertEnter", opts = {} },
     { "echasnovski/mini.surround", event = "BufEnter",    opts = {} },
     { "shaunsingh/nord.nvim",      event = "VeryLazy" },
+    {
+        'glacambre/firenvim',
+
+        -- Lazy load firenvim
+        -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+        lazy = not vim.g.started_by_firenvim,
+        build = function()
+            vim.fn["firenvim#install"](0)
+        end
+    },
+
     vim.cmd("colorscheme sainte-adresse"),
 }
--- lua/karidus/plugins/mini.lua
+++ lua/karidus/plugins/mini.lua
@@ -2,5 +2,15 @@ return {
     { 'echasnovski/mini.notify',      version = false, opts = {} },
     { 'echasnovski/mini.map',         version = '*',   opts = {} },
     { 'echasnovski/mini.indentscope', version = false, opts = {} },
-    { 'echasnovski/mini.map',         version = '*',   opts = {} },
+    {
+        'echasnovski/mini.hipatterns',
+        version = '*',
+        event = "VeryLazy",
+        opts = {
+            highlighters = {
+                done = { pattern = '%f[%w]()DONE()%f[%W]', group = 'MiniHipatternsDone' },
+            }
+
+        }
+    },
 }
