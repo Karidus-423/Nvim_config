c7c3263907e4f48b3113a99a02742ef3376e17e7
Karidus-423 <kapuerto23@gmail.com>
2025-02-04 20:54:31 +0000

    idk

---
+++ lua/karidus/neovide.lua
@@ -0,0 +1,12 @@
+if vim.g.neovide then
+	-- Put anything you want to happen only in Neovide here
+	vim.o.guifont = "Monaspace Xenon:h16"
+	vim.cmd [[colorscheme habamax]]
+	vim.g.neovide_transparency = 0.9
+	vim.g.neovide_normal_opacity = 0.9
+	vim.g.neovide_refresh_rate = 100
+	vim.g.neovide_refresh_rate_idle = 5
+	vim.g.neovide_cursor_animate_in_insert_mode = true
+	vim.g.neovide_cursor_animate_command_line = true
+	vim.g.neovide_cursor_vfx_mode = "pixiedust"
+end
