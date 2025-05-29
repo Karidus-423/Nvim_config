c640d4740a5cb373beec3e13a8e5d272f8569b9a
Karidus-423 <kapuerto23@gmail.com>
2025-05-29 03:21:29 +0000

    lua_ls use modules

--- lua/karidus/plugins/lsp.lua
+++ lua/karidus/plugins/lsp.lua
@@ -6,6 +6,33 @@ return {
 			vim.lsp.enable('clangd')
 			--Lua
 			vim.lsp.enable('lua_ls')
+			vim.lsp.config('lua_ls', {
+				settings = {
+					Lua = {
+						workspace = {
+							checkThirdParty = true,
+							library = {
+								vim.env.VIMRUNTIME,
+								-- Depending on the usage, you might want to add additional paths
+								-- here.
+								-- '${3rd}/luv/library'
+								-- '${3rd}/busted/library'
+								'/usr/share/lua/5.3',
+								'/usr/share/lua/5.4',
+								'/usr/lib/luarocks/rocks-5.4/astal',
+							}
+							-- Or pull in all of 'runtimepath'.
+							-- NOTE: this is a lot slower and will cause issues when working on
+							-- your own configuration.
+							-- See https://github.com/neovim/nvim-lspconfig/issues/3189
+							-- library = {
+							--   vim.api.nvim_get_runtime_file('', true),
+							-- }
+						}
+					}
+				}
+			})
+
 			--Zig
 			vim.lsp.enable('zls')
 			--JS/TS
